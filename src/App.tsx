df630074e2939068c48a47e3040495b1f9df4593
ArtemKhairov <artemxairoff@yandex.ru>
2023-12-22 20:23:28 +0700

    add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 //
 import { AboutPageLazy } from "./pages/About/AboutPage.async";
 import { MainPageLazy } from "./pages/Main/MainPage.async";
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>{theme}</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutPageLazy />} />
